b27cb6032fb6bcf8b0680a56ca08bfc41277dc32
charri31 <40201351+charri31@users.noreply.github.com>
2018-06-12 07:53:08 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,6 +9,10 @@ client.on('message', message => {
     if (message.content === 'Good morning, Naiz') {
     	message.reply('Good morning to you too!');
   	}
+ {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply(':blobkissheart: Im sorry! This is the best I can do!');
+  	}
 });
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);

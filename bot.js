2d7e3e127d1a2556bc20fc6d3afdded5c9a6ce39
charri31 <40201351+charri31@users.noreply.github.com>
2018-06-12 09:21:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,16 +13,12 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'Naiz, can I have a hug?') {
-    	message.reply('Im sorry! This is the best I can do! \n\ Is that alright?');
+    	message.reply('\n\ Im sorry! This is the best I can do! \n\ Is that alright?');
   	}
 });
 client.on('message', message => {
     if (message.content === 'Who are the Hana?') {
-    	message.reply('☼ **H A N A** ☼');
-    	message.reply('o Tribe Color: White/Red');
-    	message.reply('o Location: Unknown');
-    	message.reply('o Characteristics: Tolerant | Benign | Pacifists');
-    	message.reply('A splinter sect of the Kha that broke off many generations ago. They were a culmination of Xaela and Raen who followed a set of teachings emphasizing pacifism and peace. The tribe was mostly wiped out by the Garlean Empire approximately 15 years ago.');
+('\n\ ☼ **H A N A** ☼ \n\ o Tribe Color: White/Red \n\ o Location: Unknown \n\ o Characteristics: Tolerant | Benign | Pacifist \n\ A splinter sect of the Kha that broke off many generations ago. \n\ They were a culmination of Xaela and Raen who followed a set of teachings emphasizing pacifism and peace. The tribe was mostly wiped out by the Garlean Empire approximately 15 years ago.');
   	}
 });
 client.on('message', message => {

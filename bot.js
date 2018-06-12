62df457328dbd3ccb7fb7e8a5f4f931fd94c19a9
charri31 <40201351+charri31@users.noreply.github.com>
2018-06-12 08:06:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,248 @@ client.on('message', message => {
 
 client.on('message', message => {
     if (message.content === 'Naiz, can I have a hug?') {
-    	message.reply(':blobkissheart: Im sorry! This is the best I can do!');
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Who are the Hana?') {
+    	message.reply('☼ **H A N A** ☼
+o Tribe Color: White/Red
+o Location: Unknown
+o Characteristics: Tolerant | Benign | Pacifists
+A splinter sect of the Kha that broke off many generations ago.
+They were a culmination of Xaela and Raen who followed a set of teachings emphasizing pacifism and peace.
+The tribe was mostly wiped out by the Garlean Empire approximately 15 years ago.');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
+  	}
+});
+client.on('message', message => {
+    if (message.content === 'Naiz, can I have a hug?') {
+    	message.reply('Im sorry! This is the best I can do!');
   	}
 });
 // THIS  MUST  BE  THIS  WAY
